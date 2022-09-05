54874c61d8e7a24f2930e26218459cee9195de3b
glyph <glyph@mycelial.technology>
2022-09-05 10:09:00 +0100

    Revert "hide parts 2, 3 and 4"
    This reverts commit ce693cadc3bdd62a251a0e65b935e02cb5a61844.

---
+++ part_2_subscribe_form/src/sbot.rs
@@ -0,0 +1,39 @@
+use std::env;
+
+use golgi::{api::friends::RelationshipQuery, sbot::Keystore, Sbot};
+
+/// Initialise a connection to a Scuttlebutt server.
+pub async fn init_sbot() -> Result<Sbot, String> {
+    let go_sbot_port = env::var("GO_SBOT_PORT").unwrap_or_else(|_| "8021".to_string());
+
+    let keystore = Keystore::GoSbot;
+    let ip_port = Some(format!("127.0.0.1:{}", go_sbot_port));
+    let net_id = None;
+
+    Sbot::init(keystore, ip_port, net_id)
+        .await
+        .map_err(|e| e.to_string())
+}
+
+/// Return the public key of the local sbot instance.
+pub async fn whoami() -> Result<String, String> {
+    let mut sbot = init_sbot().await?;
+
+    sbot.whoami().await.map_err(|e| e.to_string())
+}
+
+/// Check follow status.
+///
+/// Is peer A (`public_key_a`) following peer B (`public_key_b`)?
+pub async fn is_following(public_key_a: &str, public_key_b: &str) -> Result<String, String> {
+    let mut sbot = init_sbot().await?;
+
+    let query = RelationshipQuery {
+        source: public_key_a.to_string(),
+        dest: public_key_b.to_string(),
+    };
+
+    sbot.friends_is_following(query)
+        .await
+        .map_err(|e| e.to_string())
+}
---
+++ part_4_posts_streams/src/main.rs
@@ -0,0 +1,25 @@
+mod db;
+mod routes;
+mod sbot;
+mod utils;
+
+use rocket::{launch, routes};
+use rocket_dyn_templates::Template;
+use xdg::BaseDirectories;
+
+use crate::{db::Database, routes::*};
+
+#[launch]
+async fn rocket() -> _ {
+    // Create the key-value database.
+    let xdg_dirs = BaseDirectories::with_prefix("lykin").unwrap();
+    let db_path = xdg_dirs
+        .place_config_file("database")
+        .expect("cannot create database directory");
+    let db = Database::init(&db_path);
+
+    rocket::build()
+        .manage(db)
+        .attach(Template::fairing())
+        .mount("/", routes![home, subscribe_form, unsubscribe_form])
+}
